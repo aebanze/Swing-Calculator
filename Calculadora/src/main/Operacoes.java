ccdeea140537b1285b63a5dddc30f0b632da5f82
Angel Banze <angeleliasanjo@gmail.com>
2021-09-12 12:44:10 -0700

    adding the main project

---
+++ Calculadora/src/main/Operacoes.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Banze
+ */
+public class Operacoes {
+    
+    public float soma (float num1, float num2){
+        float res = num1 + num2;
+        return res;
+    }
+    
+    public float subtr (float num1, float num2){
+        float res = num1 - num2;
+        return res;
+    }
+    
+    public float multli (float num1, float num2){
+        float res = num1 * num2;
+        return res;
+    }
+    
+    public float div (float num1, float num2){
+        float res = num1 / num2;
+        return res;
+    }
+    
+}
